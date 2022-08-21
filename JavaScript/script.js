434007a77887d3788c754af1a903ea7416e73642
Haris <mharispk321@gmail.com>
2022-08-21 23:53:14 +0500

    Add files via upload
    Initial Commit

---
+++ JavaScript/script.js
@@ -0,0 +1,9 @@
+// typing text animation script
+var typed = new Typed(".typing", {
+  strings: ["Engineer", "Front-End Developer", "Database Developer", "Blogger"],
+  typeSpeed: 90,
+  backSpeed: 40,
+  cursorChar: " |",
+  smartBackspace: true,
+  loop: true,
+});
