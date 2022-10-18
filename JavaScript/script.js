d458a570fde5d0dac55ab92e34e5b5227500fd8a
Nupur Jeswani <76174923+nupur-jeswani@users.noreply.github.com>
2022-10-19 02:06:38 +0530

    Feat add dark mode.
    Added dark mode toggle functionality to an icon.

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -7,3 +7,15 @@ var typed = new Typed(".typing", {
   smartBackspace: true,
   loop: true,
 });
+
+var icon = document.getElementById("icon");
+
+icon.onclick = function() {
+  document.body.classList.toggle("dark-theme");
+  
+  if(document.body.classList.contains("dark-theme")) {
+    icon.src = "Images/sun.png";
+  } else {
+    icon.src = "Images/moon.png";
+  }
+}
